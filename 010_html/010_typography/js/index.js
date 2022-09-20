8d49e20fb1c940c7cb3a54749338a44352e0c989
vmarshirov <vmarshirov@outlook.com>
2022-09-20 17:33:09 +0300

    dev_branch

---
+++ 010_html/010_typography/js/index.js
@@ -0,0 +1,25 @@
+;
+window.alert("Оповещение из js-файла в рабочее окно");
+console.log("Оповещение из js-файла в  в консоль");
+
+const p_3_child_0 = document.querySelectorAll('p')[3].children[0]
+p_3_child_0.style = "font-weight: bold; font-size: 4.5rem; color: brown; background-color: aquamarine;   word-spacing: 3rem; letter-spacing: 3.0rem;"
+
+
+if (window.confirm("Изменить стиль на новый?")) {
+    p_3_child_0.style = "font-weight: bold; font-size: 4.5rem; color: red; background-color: aquamarine;   word-spacing: 3rem; letter-spacing: 3.0rem;"
+}
+
+function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+}
+
+function new_color() {
+    var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    p_3_child_0.style.color = rndCol;
+}
+
+
+p_3_child_0.addEventListener("mouseleave", new_color, false)
+// https://developer.mozilla.org/en-US/docs/Web/Events
+// https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
