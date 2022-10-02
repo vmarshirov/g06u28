9042936989a6d06cca60a16537dc30a59ced3d22
vmarshirov <vmarshirov@outlook.com>
2022-10-02 13:02:24 +0300

    api

--- 030_js/js/0620_api.js
+++ 030_js/js/0620_api.js
@@ -1,11 +1,24 @@
 // https://www.youtube.com/watch?v=QSF8JXFPR18&list=PLiOhsP3M5j5wIUlXkYMuWTVR8inofxLS0&index=7
 async function getResponce() {
     // let responce = await fetch("https://my-json-server.typicode.com/typicode/demo/posts")
-    let responce = await fetch("https://vmarshirov.github.io/g06u28/030_js/data/0620.json")
-    /*
-        let content = await responce.text()
-        console.log(JSON.parse(content))
-    */
+    //let responce = await fetch("https://vmarshirov.github.io/g06u28/030_js/data/0620.json")
+    let responce = await fetch("http://185.182.111.214:7633/tmp/g06u28.txt_api.json")
+    //let responce = await fetch("http://185.182.111.214:7633/tmp/index_api.txt_only.json")
+
+/*
+
+    if (response.ok) { // if HTTP-status is 200-299
+                       // get the response body (the method explained below)
+        let content = await response.json();
+    } else {
+        alert("HTTP-Error: " + response.status);
+    }
+*/
+
+
+        // let content = await responce.text()
+        // console.log(JSON.parse(content))
+
     let content = await responce.json()
     content = content.splice(0, 5)
     console.log(content)
