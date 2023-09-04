b66c42ab3395cd0ed4791a357127fbe5f13ca777
vmarshirov <vmarshirov@outlook.com>
2023-09-04 20:18:18 +0300

    e

--- 010_html/js/010_typography.js
+++ 010_html/js/010_typography.js
@@ -1,6 +1,7 @@
 alert("Запускаем скрипт")
 let name  = document.getElementsByTagName('p')[2].childNodes[1].innerText
 const node_1 = document.getElementsByTagName('p')[1].childNodes[1]
-console.log(node_1.innerHTML)
+console.log(name
+)
 node_1.innerHTML = '123www'
 console.log(node_1.innerHTML)
