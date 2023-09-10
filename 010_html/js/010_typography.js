55dbd9d88afc1a7b0814b117326abc525fbf459a
vmarshirov <vmarshirov@outlook.com>
2023-09-10 16:26:25 +0300

    e

--- 010_html/js/010_typography.js
+++ 010_html/js/010_typography.js
@@ -1,7 +1,9 @@
-alert("Запускаем скрипт")
-let name  = document.getElementsByTagName('p')[2].childNodes[1].innerText
-const node_1 = document.getElementsByTagName('p')[1].childNodes[1]
-console.log(name
-)
-node_1.innerHTML = '123www'
-console.log(node_1.innerHTML)
+const node_for_click = document.getElementById("for_click")
+
+function find_edit(){
+    const item  = document.getElementsByTagName('p')[1]
+    console.log(item.innerText)
+    item.innerText = 'новое значение'
+}
+
+node_for_click.addEventListener("click",find_edit)
---
+++ g.sh
@@ -0,0 +1,5 @@
+git pull
+git status
+git add .
+git commit -me''
+git push
