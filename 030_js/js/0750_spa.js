5fde5f69372a6ebbb5e13957666fe6c7077e6ae5
vmarshirov <vmarshirov@outlook.com>
2023-10-30 13:34:50 +0300

    e

--- 030_js/js/0750_spa.js
+++ 030_js/js/0750_spa.js
@@ -21,7 +21,7 @@ function nav_clear() {
     component.innerHTML = content
 }*/
 
-async function fetchHTML_01() {
+function fetchHTML_01() {
 //console.clear()
 baseUrl = document.URL
 console.log("baseUrl: ", baseUrl)
