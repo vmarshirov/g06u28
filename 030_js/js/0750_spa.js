0c0a1df6de843cca3d60a0a4cf3df0d0d5b0b2c2
vmarshirov <vmarshirov@outlook.com>
2023-10-30 10:08:31 +0300

    e

--- 030_js/js/0750_spa.js
+++ 030_js/js/0750_spa.js
@@ -12,13 +12,49 @@ function nav_clear() {
 }
 
 // component_01
-async function fetchHTML_01() {
-    let responce = await fetch("../../tmp/g06u28.component_01.html")
-    console.log(responce)
+/*async function fetchHTML_01() {
+//    page = '../../tmp/g06u28/' + search_obj['page']
+    let responce = await fetch("../../tmp/g06u28.component_02.html")
     let content = await responce.text()
-    console.log(content)
+    console.log("content: ", content)
     let component = document.getElementById("component")
     component.innerHTML = content
+}*/
+
+async function fetchHTML_01() {
+console.clear()
+baseUrl = document.URL
+console.log("baseUrl: ", baseUrl)
+const url = new URL(baseUrl)
+console.log("url: ", url)
+console.log("url.pathname: ", url.pathname)
+path_lst  = url.pathname.split("/")
+last_path = path_lst[path_lst.length-1]
+console.log("last_path: ", last_path)
+console.log("url.search: ", url.search)
+search_string = url.search.split("?")
+search_string[1].split("&")
+search_parameters = search_string[1].split("&")
+console.log("search_parameters:", search_parameters)
+search_obj = {}
+for (let index = 0; index < search_parameters.length; index++) {
+    pair = search_parameters[index].split("=")
+    key = pair[0]; value = pair[1]
+    search_obj[key] = value
+    console.log(search_obj)
+}
+console.log("search_obj: ", search_obj)
+
+  page1 = url.hostname + url.pathname.slice(0,-14) + "/pages/" + search_obj['page']
+//   page1 = url.href.slice(0,-37) + "/pages/" + search_obj['page']//   page1 =  "../../tmp/" + search_obj['page']
+   console.log("page1: ", page1)
+    let responce = await fetch(page1)
+//    ../../tmp/g06u28.component_02.html
+    let content = await responce.text()
+    console.log("content: ", content)
+    let component = document.getElementById("component")
+    component.innerHTML = content
+
 }
 
 function navComponent_01() {
@@ -26,9 +62,10 @@ function navComponent_01() {
     nav_01.style = "color: red;"
 }
 
-function Component_01() {
-    fetchHTML_01()
+async function Component_01() {
     navComponent_01()
+    fetchHTML_01()
+
 }
 
 
@@ -36,7 +73,7 @@ function Component_01() {
 async function fetchHTML_02() {
     let responce = await fetch("../../tmp/g06u28.component_02.html")
     let content = await responce.text()
-    console.log(content)
+    console.log("content: ", content)
     let component = document.getElementById("component")
     component.innerHTML = content
 
@@ -48,7 +85,7 @@ async function fetchJson_02() {
     //let responce = await fetch("http://185.182.111.214:7628/tmp/g06u28.txt_api.json")
     let responce = await fetch("../../tmp/g06u28.shop.json")
     let content = await responce.text()
-    console.log(content)
+    console.log("content: ", content)
     content = JSON.parse(content)
     // content = await responce.json()
     content = content.splice(0, 4)
@@ -80,12 +117,17 @@ function navComponent_02() {
 }
 
 function Component_02() {
+    navComponent_02()
     fetchHTML_02()
     fetchJson_02()
-    navComponent_02()
+
 }
+
 
 const nav_01 = document.getElementById("id_nav_01")
 nav_01.addEventListener("click", Component_01)
 const nav_02 = document.getElementById("id_nav_02")
 nav_02.addEventListener("click", Component_02)
+
+
+
