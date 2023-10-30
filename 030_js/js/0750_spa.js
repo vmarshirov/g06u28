c1f72ee022e160292c19622cf165d9f138b92beb
vmarshirov <vmarshirov@outlook.com>
2023-10-30 21:18:19 +0300

    e

--- 030_js/js/0750_spa.js
+++ 030_js/js/0750_spa.js
@@ -117,7 +117,7 @@ console.clear()
         console.log(search_obj)
     }
    console.log("search_obj: ", search_obj)
-   var content_url  = "https://vmarshirov.github.io/g06u28/030_js/public_html/pages/" + 'component_01.html'
+   var content_url  = "https://vmarshirov.github.io/g06u28/030_js/public_html/pages/" + search_obj
     console.log("content_url: ", content_url)
 
 const nav_01 = document.getElementById("id_nav_01")
