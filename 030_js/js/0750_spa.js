3bdff45899323434d315e3a8ff0484bd237869b2
vmarshirov <vmarshirov@outlook.com>
2023-11-01 19:10:45 +0300

    e

--- 030_js/js/0750_spa.js
+++ 030_js/js/0750_spa.js
@@ -2,23 +2,24 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 // https://jsonplaceholder.typicode.com/
 
-function nav_clear(page) {
-    const nav_items = document.getElementsByClassName("nav-link")
+
+function activeNavigTitle(content_file_key) {
+    const nav_items = document.getElementsByClassName("nav-item")
     for (let index = 0; index < nav_items.length; index++) {
         console.log("index: ", index)
-        console.log("page: ", page)
-        nav_items[index].style = "color: green;"
-        if(nav_items[index].id == page) {
-        console.log('nav_items[index].id: ', nav_items[index].id)
-        console.log("innerText: ", nav_items[index].innerText)
-        document.getElementsByTagName("title")[0].innerText=nav_items[index].innerText
-        nav_items[index].style = "color: red;"
+        console.log("content_file_key: ", content_file_key)
+        console.log("nav_items[index].childNodes[0].className: ", nav_items[index].childNodes[0].className)
+        nav_items[index].childNodes[0].className = "nav-link"
+        if(nav_items[index].childNodes[0].id == content_file_key) {
+            nav_items[index].childNodes[0].className = "nav-link active"
+            console.log('nav_items[index].id: ', nav_items[index].id)
+            console.log("innerText: ", nav_items[index].innerText)
+            document.getElementsByTagName("title")[0].innerText=nav_items[index].innerText
         }
-
     }
 }
 
-async function fetchHTML_01(content_url) {
+async function fetchHTML(content_url) {
    console.log("content_url: ", content_url)
     let response = await fetch(content_url)
     let content = await response.text()
@@ -26,28 +27,21 @@ async function fetchHTML_01(content_url) {
     let component = document.getElementById("component")
     component.innerHTML = content
 }
-function navComponent_01(page) {
-    nav_clear(page)
-    nav_01.style = "color: red;"
-}
-function Component_01(content_url, page) {
-    fetchHTML_01(content_url)
-    navComponent_01(page)
+
+function Component(content_url, content_file_key) {
+    activeNavigTitle(content_file_key)
+    fetchHTML(content_url)
 }
 
 //------------------------------
-/*function navComponent_03() {
-    nav_clear()
-    nav_03.style = "color: red;"
-}*/
-async function fetchJson_03() {
-    let component = document.getElementById("component")
-    let innerHTML = '<h6>component_03</h6>\n<p>Содержание ...</p>\n\n'
 
+async function fetchJson() {
+    let component = document.getElementById("component")
+    let innerHTML = '<h6>component_json_</h6>\n<p>Содержание ...</p>\n\n'
     // let responce = await fetch("https://my-json-server.typicode.com/typicode/demo/posts")
     //let responce = await fetch("https://vmarshirov.github.io/g06u28/030_js/data/0620.json")
     //let responce = await fetch("http://185.182.111.214:7628/tmp/g06u28.txt_api.json")
-    let responce = await fetch("https://vmarshirov.github.io/g06u28/030_js/public_html/pages/shop.json")
+    let responce = await fetch("../public_html/pages/component_json.json")
     let content = await responce.text()
     console.log("content: ", content)
     content = JSON.parse(content)
@@ -84,20 +78,19 @@ async function fetchJson_03() {
     console.log(innerHTML)
     component.innerHTML = innerHTML
 }
-function Component_03(content_url, page) {
-    fetchJson_03()
-    navComponent_01(page)
+
+function component_json(content_url, content_file_key) {
+    activeNavigTitle(content_file_key)
+    fetchJson()
 }
 
+
+//По URL названия файлов для считывания  content_file_key
 console.clear()
 baseUrl = document.URL
 console.log("baseUrl: ", baseUrl)
 const url = new URL(baseUrl)
 console.log("url: ", url)
-console.log("url.pathname: ", url.pathname)
-path_lst  = url.pathname.split("/")
-last_path = path_lst[path_lst.length-1]
-console.log("last_path: ", last_path)
 console.log("url.search: ", url.search)
 search_string = url.search.split("?")
 search_string[1].split("&")
@@ -107,27 +100,31 @@ search_obj = {}
 for (let index = 0; index < search_parameters.length; index++) {
     pair = search_parameters[index].split("=")
     key = pair[0]; value = pair[1]
-    search_obj[key] = value
-    console.log(search_obj)
+    search_obj[key] = value  //формируем объект из строки запроса
 }
-console.log("search_obj.page: ", search_obj.page)
-var content_url  = "../public_html/pages/" + search_obj.page
-console.log("content_url: ", content_url)
+console.log("search_obj: ",search_obj) //объект из строки запроса
+console.log("search_obj.content_file_key: ", search_obj.content_file_key)
+var content_url  = "../public_html/pages/" + search_obj.content_file_key // формируем путь к файлу для считывания
+console.log("content_url: ", content_url) // путь к файлу для считывания
 
-if(search_obj.page ==  "component_03.html"){
-Component_03(content_url, search_obj.page)
+//Вызываем основные функции
+console.log("content_url, search_obj.content_file_key: ", content_url, search_obj.content_file_key)
+if(search_obj.content_file_key !=  "component_json.json"){
+Component(content_url, search_obj.content_file_key)
 }
-else{Component_01(content_url, search_obj.page)}
+else{
+component_json(content_url, search_obj.content_file_key)
+}
 
 
 /*
 
 const nav_01 = document.getElementById("nav_01_id")
-nav_01.addEventListener("click", Component_01())
+nav_01.addEventListener("click", Component())
 const nav_02 = document.getElementById("nav_02_id")
-nav_02.addEventListener("click", Component_01())
+nav_02.addEventListener("click", Component())
 const nav_03 = document.getElementById("nav_03_id")
-nav_03.addEventListener("click", Component_03())
+nav_03.addEventListener("click", component_json())
 
 
 */
