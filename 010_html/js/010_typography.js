0b2081370b50d3d5b5f1dc064f098ba3c5825b24
vmarshirov <vmarshirov@outlook.com>
2024-10-23 17:31:39 +0300

    ed

--- 010_html/js/010_typography.js
+++ 010_html/js/010_typography.js
@@ -1,9 +1,8 @@
-const node_for_click = document.getElementById("for_click")
-
 function find_edit(){
     const item  = document.getElementById('new')
     console.log(item.innerText)
-    item.innerHTML="<b>новый фрагмент</b>"
+    item.innerHTML="<b>оригинальный фрагмент изменен</b>"
 }
 
+const node_for_click = document.getElementById("for_click")
 node_for_click.addEventListener("click",find_edit)
